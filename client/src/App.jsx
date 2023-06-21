ac915265b5f6df91965d281ec48a2c09772dfbeb
AliNormohammmadzadeh <104034996+AliNormohammmadzadeh@users.noreply.github.com>
2023-06-21 11:29:16 +0430

    first or last

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Customizer from "./pages/Customizer"
+import CanvasModel from './canvas'
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <CanvasModel/>
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
